abfc2386b3cb783308287169ddaa8cfe8cb3bef2
Alisher Turubekov <develepor@mail.ru>
2023-10-08 21:57:00 +0600

    php mailer send

--- js/script.js
+++ js/script.js
@@ -95,5 +95,25 @@ $(document).ready(function(){
     validateForm('.feed-form')
 
     $('input[name=phone]').mask("+7(999)999-99-99");
+
+    $('form').submit(function(e){
+      e.preventDefault();
+      if(!$(this).valid()){
+          return;
+      }
+      $.ajax({
+          type: 'POST',
+          url: 'mailer/smart.php',
+          data: $(this).serialize()
+      }).done(function() {
+          $(this).find("input").val("");
+
+          // $('#consultation').fadeOut();
+          // $('.overlay, #thanks').fadeIn('slow');
+
+          $('form').trigger('reset');
+      }); 
+      return false;
+    });
   
 }); 
